d41fd01870323a176172132cdd614632f65326eb
tha-springer-dude <willie.springer@gmail.com>
2025-05-12 14:57:56 +0200

    upload via browser

---
+++ 8-curl/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$ch = curl_init();
+
+//curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt_array($ch, [
+    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_RETURNTRANSFER => true
+]);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+echo $response, "\n";
