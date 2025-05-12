53266e219f50ef7f173ee54a2098f3d17bfa0b72
tha-springer-dude <willie.springer@gmail.com>
2025-05-12 15:59:56 +0200

    uploaded via browser

---
+++ 10-request_headers/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$ch = curl_init();
+
+$headers = [
+    "Authorization: Client-ID YOUR_ACCESS_KEY"
+];
+
+curl_setopt_array($ch, [
+    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $headers
+]);
+
+$response = curl_exec($ch);
+
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+echo $status_code, "\n";
+
+echo $response, "\n";
