152ea448c22c5bf519bd8b4d92867bd79932c547
Willie <willie.springer@gmail.com>
2025-06-23 15:59:59 +0200

    Stripe API implemented

--- 19.Use an SDK: compare the Stripe API to its SDK/index.php
+++ 19.Use an SDK: compare the Stripe API to its SDK/index.php
@@ -1,10 +1,19 @@
 <?php
 
+$api_key = "YOUR_API_KEY";
+
+$data = [
+    "name" => "Alice",
+    "email" => "alice@example.com"
+];
+
 $ch = curl_init();
 
 curl_setopt_array($ch, [
     CURLOPT_URL => 'https://api.stripe.com/v1/customers',
     CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_USERPWD => $api_key,
+    CURLOPT_POSTFIELDS => http_build_query($data)
 ]);
 
 $response = $curl_exec($ch);
